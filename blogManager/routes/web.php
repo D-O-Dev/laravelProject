435065d727a05340b895f815731213cb65e6af50
D-O-Dev <daniel1.grah@epitech.eu>
2026-04-24 13:07:49 +0000

    [RANGE]Suppression de tous les fichiers de laravel breeze pour integrer et ajouter mes propre pages

--- blogManager/routes/web.php
+++ blogManager/routes/web.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/dashboard', function () {
